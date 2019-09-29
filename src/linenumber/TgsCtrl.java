837ee1b5ff164b33bab2f8c3b3e3d7626df52878
mgrvp <mohgp855@gmail.com>
2019-09-29 22:05:02 +0700

    Buat GUI

---
+++ src/linenumber/TgsCtrl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linenumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TgsCtrl {
+    
+    private LineNumberTugas view;
+    private List<Integer> list = new ArrayList<>();
+    
+    public TgsCtrl (LineNumberTugas view){
+        
+    }
+    
+}
